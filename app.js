f8cf4f9d21cdb299dba190c47fab1d87d211b941
yichennn <yichennn1010@gmail.com>
2022-12-27 12:36:04 +0800

    add login and register route and page add todo and user model & add relations

--- app.js
+++ app.js
@@ -5,15 +5,46 @@ const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
 
+const db = require('./models')
+const User = db.User
+const Todo = db.Todo
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+// ======== 路由 ======== //
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+// ======== 登入 ======== //
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+// ======== 註冊 ======== //
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password, confirmPassword })
+    .then(() => res.redirect('/'))
+})
+
+// ======== 登出 ======== //
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
+
+// ======== 監聽 ======== //
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
 })
