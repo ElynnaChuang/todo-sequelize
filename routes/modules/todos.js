3141f464f5ac9980659ece4e2541bbaa197058b4
yichennn <yichennn1010@gmail.com>
2022-12-29 19:07:54 +0800

    feature : add create todo

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,12 +4,31 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-// ======== 詳細頁 ======== //
+// ======== C 新增todo ======== //
+router.get('/new', (req, res) => {
+  res.render('new')
+})
+
+router.post('/', (req, res) => {
+  const { name } = req.body
+  const UserId = req.user.id
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch((err) => {
+      console.log(err)
+      res.render('error', { err })
+    })
+})
+
+// ======== R詳細頁 ======== //
 router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
-
+    .catch((err) => {
+      console.log(err)
+      res.render('error', { err })
+    })
 })
 
 module.exports = router
